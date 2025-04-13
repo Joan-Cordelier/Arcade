1298fa05fc5638bd4fe62e6e0746799d65ce9ccf
joan cordelier <joan.cordelier@epitech.eu>
2025-04-13 23:17:15 +0200

    ADD: make cases on one line

--- Display/ncurses/NCurse.cpp
+++ Display/ncurses/NCurse.cpp
@@ -122,324 +122,85 @@ Event NcurseDisplayer::pollEvent()
     nodelay(_window, FALSE);
     
     if (key == ERR) {
-        return {
-            EventType::NONE,
-            Key::NONE,
-            {-1, -1, -1}
-        };
+        return {EventType::NONE, Key::NONE, {-1, -1, -1}};
     }
     
     switch (key) {
-        case KEY_UP:
-            return {
-                EventType::KEY_PRESSED,
-                Key::UP,
-                {-1, -1, -1}
-            };
-        case KEY_DOWN:
-            return {
-                EventType::KEY_PRESSED,
-                Key::DOWN,
-                {-1, -1, -1}
-            };
-        case KEY_LEFT:
-            return {
-                EventType::KEY_PRESSED,
-                Key::LEFT,
-                {-1, -1, -1}
-            };
-        case KEY_RIGHT:
-            return {
-                EventType::KEY_PRESSED,
-                Key::RIGHT,
-                {-1, -1, -1}
-            };
+        case KEY_UP: return {EventType::KEY_PRESSED, Key::UP, {-1, -1, -1}};
+        case KEY_DOWN: return {EventType::KEY_PRESSED, Key::DOWN, {-1, -1, -1}};
+        case KEY_LEFT: return {EventType::KEY_PRESSED, Key::LEFT, {-1, -1, -1}};
+        case KEY_RIGHT: return {EventType::KEY_PRESSED, Key::RIGHT, {-1, -1, -1}};
         case KEY_ENTER:
-        case 10:
-            return {
-                EventType::KEY_PRESSED,
-                Key::ENTER,
-                {-1, -1, -1}
-            };
-        case 27:
-            return {
-                EventType::KEY_PRESSED,
-                Key::ESCAPE,
-                {-1, -1, -1}
-            };
-        case 32:
-            return {
-                EventType::KEY_PRESSED,
-                Key::SPACE,
-                {-1, -1, -1}
-            };
+        case 10: return {EventType::KEY_PRESSED, Key::ENTER, {-1, -1, -1}};
+        case 27: return {EventType::KEY_PRESSED, Key::ESCAPE, {-1, -1, -1}};
+        case 32: return {EventType::KEY_PRESSED, Key::SPACE, {-1, -1, -1}};
         case KEY_BACKSPACE:
-        case 127:
-            return {
-                EventType::KEY_PRESSED,
-                Key::BACKSPACE,
-                {-1, -1, -1}
-            };
-        case KEY_F(1):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F1,
-                {-1, -1, -1}
-            };
-        case KEY_F(2):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F2,
-                {-1, -1, -1}
-            };
-        case KEY_F(3):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F3,
-                {-1, -1, -1}
-            };
-        case KEY_F(4):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F4,
-                {-1, -1, -1}
-            };
-        case KEY_F(5):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F5,
-                {-1, -1, -1}
-            };
-        case KEY_F(6):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F6,
-                {-1, -1, -1}
-            };
-        case KEY_F(7):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F7,
-                {-1, -1, -1}
-            };
-        case KEY_F(8):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F8,
-                {-1, -1, -1}
-            };
-        case KEY_F(9):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F9,
-                {-1, -1, -1}
-            };
-        case KEY_F(10):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F10,
-                {-1, -1, -1}
-            };
-        case KEY_F(11):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F11,
-                {-1, -1, -1}
-            };
-        case KEY_F(12):
-            return {
-                EventType::KEY_PRESSED,
-                Key::F12,
-                {-1, -1, -1}
-            };
+        case 127: return {EventType::KEY_PRESSED, Key::BACKSPACE, {-1, -1, -1}};
+        case KEY_F(1): return {EventType::KEY_PRESSED, Key::F1, {-1, -1, -1}};
+        case KEY_F(2): return {EventType::KEY_PRESSED, Key::F2, {-1, -1, -1}};
+        case KEY_F(3): return {EventType::KEY_PRESSED, Key::F3, {-1, -1, -1}};
+        case KEY_F(4): return {EventType::KEY_PRESSED, Key::F4, {-1, -1, -1}};
+        case KEY_F(5): return {EventType::KEY_PRESSED, Key::F5, {-1, -1, -1}};
+        case KEY_F(6): return {EventType::KEY_PRESSED, Key::F6, {-1, -1, -1}};
+        case KEY_F(7): return {EventType::KEY_PRESSED, Key::F7, {-1, -1, -1}};
+        case KEY_F(8): return {EventType::KEY_PRESSED, Key::F8, {-1, -1, -1}};
+        case KEY_F(9): return {EventType::KEY_PRESSED, Key::F9, {-1, -1, -1}};
+        case KEY_F(10): return {EventType::KEY_PRESSED, Key::F10, {-1, -1, -1}};
+        case KEY_F(11): return {EventType::KEY_PRESSED, Key::F11, {-1, -1, -1}};
+        case KEY_F(12): return {EventType::KEY_PRESSED, Key::F12, {-1, -1, -1}};
         case 'a':
-        case 'A':
-            return {
-                EventType::KEY_PRESSED,
-                Key::A,
-                {-1, -1, -1}
-            };
+        case 'A': return {EventType::KEY_PRESSED, Key::A, {-1, -1, -1}};
         case 'b':
-        case 'B':
-            return {
-                EventType::KEY_PRESSED,
-                Key::B,
-                {-1, -1, -1}
-            };
+        case 'B': return {EventType::KEY_PRESSED, Key::B, {-1, -1, -1}};
         case 'c':
-        case 'C':
-            return {
-                EventType::KEY_PRESSED,
-                Key::C,
-                {-1, -1, -1}
-            };
+        case 'C': return {EventType::KEY_PRESSED, Key::C, {-1, -1, -1}};
         case 'd':
-        case 'D':
-            return {
-                EventType::KEY_PRESSED,
-                Key::D,
-                {-1, -1, -1}
-            };
+        case 'D': return {EventType::KEY_PRESSED, Key::D, {-1, -1, -1}};
         case 'e':
-        case 'E':
-            return {
-                EventType::KEY_PRESSED,
-                Key::E,
-                {-1, -1, -1}
-            };
+        case 'E': return {EventType::KEY_PRESSED, Key::E, {-1, -1, -1}};
         case 'f':
-        case 'F':
-            return {
-                EventType::KEY_PRESSED,
-                Key::F,
-                {-1, -1, -1}
-            };
+        case 'F': return {EventType::KEY_PRESSED, Key::F, {-1, -1, -1}};
         case 'g':
-        case 'G':
-            return {
-                EventType::KEY_PRESSED,
-                Key::G,
-                {-1, -1, -1}
-            };
+        case 'G': return {EventType::KEY_PRESSED, Key::G, {-1, -1, -1}};
         case 'h':
-        case 'H':
-            return {
-                EventType::KEY_PRESSED,
-                Key::H,
-                {-1, -1, -1}
-            };
+        case 'H': return {EventType::KEY_PRESSED, Key::H, {-1, -1, -1}};
         case 'i':
-        case 'I':
-            return {
-                EventType::KEY_PRESSED,
-                Key::I,
-                {-1, -1, -1}
-            };
+        case 'I': return {EventType::KEY_PRESSED, Key::I, {-1, -1, -1}};
         case 'j':
-        case 'J':
-            return {
-                EventType::KEY_PRESSED,
-                Key::J,
-                {-1, -1, -1}
-            };
+        case 'J': return {EventType::KEY_PRESSED, Key::J, {-1, -1, -1}};
         case 'k':
-        case 'K':
-            return {
-                EventType::KEY_PRESSED,
-                Key::K,
-                {-1, -1, -1}
-            };
+        case 'K': return {EventType::KEY_PRESSED, Key::K, {-1, -1, -1}};
         case 'l':
-        case 'L':
-            return {
-                EventType::KEY_PRESSED,
-                Key::L,
-                {-1, -1, -1}
-            };
+        case 'L': return {EventType::KEY_PRESSED, Key::L, {-1, -1, -1}};
         case 'm':
-        case 'M':
-            return {
-                EventType::KEY_PRESSED,
-                Key::M,
-                {-1, -1, -1}
-            };
+        case 'M': return {EventType::KEY_PRESSED, Key::M, {-1, -1, -1}};
         case 'n':
-        case 'N':
-            return {
-                EventType::KEY_PRESSED,
-                Key::N,
-                {-1, -1, -1}
-            };
+        case 'N': return {EventType::KEY_PRESSED, Key::N, {-1, -1, -1}};
         case 'o':
-        case 'O':
-            return {
-                EventType::KEY_PRESSED,
-                Key::O,
-                {-1, -1, -1}
-            };
+        case 'O': return {EventType::KEY_PRESSED, Key::O, {-1, -1, -1}};
         case 'p':
-        case 'P':
-            return {
-                EventType::KEY_PRESSED,
-                Key::P,
-                {-1, -1, -1}
-            };
+        case 'P': return {EventType::KEY_PRESSED, Key::P, {-1, -1, -1}};
         case 'q':
-        case 'Q':
-            return {
-                EventType::KEY_PRESSED,
-                Key::Q,
-                {-1, -1, -1}
-            };
+        case 'Q': return {EventType::KEY_PRESSED, Key::Q, {-1, -1, -1}};
         case 'r':
-        case 'R':
-            return {
-                EventType::KEY_PRESSED,
-                Key::R,
-                {-1, -1, -1}
-            };
+        case 'R': return {EventType::KEY_PRESSED, Key::R, {-1, -1, -1}};
         case 's':
-        case 'S':
-            return {
-                EventType::KEY_PRESSED,
-                Key::S,
-                {-1, -1, -1}
-            };
+        case 'S': return {EventType::KEY_PRESSED, Key::S, {-1, -1, -1}};
         case 't':
-        case 'T':
-            return {
-                EventType::KEY_PRESSED,
-                Key::T,
-                {-1, -1, -1}
-            };
+        case 'T': return {EventType::KEY_PRESSED, Key::T, {-1, -1, -1}};
         case 'u':
-        case 'U':
-            return {
-                EventType::KEY_PRESSED,
-                Key::U,
-                {-1, -1, -1}
-            };
+        case 'U': return {EventType::KEY_PRESSED, Key::U, {-1, -1, -1}};
         case 'v':
-        case 'V':
-            return {
-                EventType::KEY_PRESSED,
-                Key::V,
-                {-1, -1, -1}
-            };
+        case 'V': return {EventType::KEY_PRESSED, Key::V, {-1, -1, -1}};
         case 'w':
-        case 'W':
-            return {
-                EventType::KEY_PRESSED,
-                Key::W,
-                {-1, -1, -1}
-            };
+        case 'W': return {EventType::KEY_PRESSED, Key::W, {-1, -1, -1}};
         case 'x':
-        case 'X':
-            return {
-                EventType::KEY_PRESSED,
-                Key::X,
-                {-1, -1, -1}
-            };
+        case 'X': return {EventType::KEY_PRESSED, Key::X, {-1, -1, -1}};
         case 'y':
-        case 'Y':
-            return {
-                EventType::KEY_PRESSED,
-                Key::Y,
-                {-1, -1, -1}
-            };
+        case 'Y': return {EventType::KEY_PRESSED, Key::Y, {-1, -1, -1}};
         case 'z':
-        case 'Z':
-            return {
-                EventType::KEY_PRESSED,
-                Key::Z,
-                {-1, -1, -1}
-            };
-        default:
-            return {
-                EventType::NONE,
-                Key::NONE,
-                {-1, -1, -1}
-            };
+        case 'Z': return {EventType::KEY_PRESSED, Key::Z, {-1, -1, -1}};
+        default: return {EventType::NONE, Key::NONE, {-1, -1, -1}};
     }
 }
 
--- Display/sdl2/SDL2.cpp
+++ Display/sdl2/SDL2.cpp
@@ -105,282 +105,52 @@ Event SDL2::pollEvent()
         if (event.type == SDL_KEYDOWN) {
             SDL_Keycode keycode = event.key.keysym.sym;
             switch (keycode) {
-                case SDLK_UP:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::UP,
-                        {-1, -1, -1}
-                    };
-                case SDLK_DOWN:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::DOWN,
-                        {-1, -1, -1}
-                    };
-                case SDLK_LEFT:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::LEFT,
-                        {-1, -1, -1}
-                    };
-                case SDLK_RIGHT:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::RIGHT,
-                        {-1, -1, -1}
-                    };                
-                case SDLK_SPACE:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::SPACE,
-                        {-1, -1, -1}
-                    };
-                case SDLK_RETURN:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::ENTER,
-                        {-1, -1, -1}
-                    };
-                case SDLK_ESCAPE:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::ESCAPE,
-                        {-1, -1, -1}
-                    };
-                case SDLK_BACKSPACE:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::BACKSPACE,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F1:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F1,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F2:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F2,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F3:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F3,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F4:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F4,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F5:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F5,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F6:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F6,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F7:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F7,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F8:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F8,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F9:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F9,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F10:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F10,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F11:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F11,
-                        {-1, -1, -1}
-                    };
-                case SDLK_F12:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F12,
-                        {-1, -1, -1}
-                    };
-                case SDLK_a:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::A,
-                        {-1, -1, -1}
-                    };
-                case SDLK_b:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::B,
-                        {-1, -1, -1}
-                    };
-                case SDLK_c:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::C,
-                        {-1, -1, -1}
-                    };
-                case SDLK_d:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::D,
-                        {-1, -1, -1}
-                    };
-                case SDLK_e:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::E,
-                        {-1, -1, -1}
-                    };
-                case SDLK_f:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::F,
-                        {-1, -1, -1}
-                    };
-                case SDLK_g:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::G,
-                        {-1, -1, -1}
-                    };
-                case SDLK_h:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::H,
-                        {-1, -1, -1}
-                    };
-                case SDLK_i:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::I,
-                        {-1, -1, -1}
-                    };
-                case SDLK_j:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::J,
-                        {-1, -1, -1}
-                    };
-                case SDLK_k:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::K,
-                        {-1, -1, -1}
-                    };
-                case SDLK_l:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::L,
-                        {-1, -1, -1}
-                    };
-                case SDLK_m:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::M,
-                        {-1, -1, -1}
-                    };
-                case SDLK_n:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::N,
-                        {-1, -1, -1}
-                    };
-                case SDLK_o:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::O,
-                        {-1, -1, -1}
-                    };
-                case SDLK_p:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::P,
-                        {-1, -1, -1}
-                    };
-                case SDLK_q:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::Q,
-                        {-1, -1, -1}
-                    };
-                case SDLK_r:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::R,
-                        {-1, -1, -1}
-                    };
-                case SDLK_s:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::S,
-                        {-1, -1, -1}
-                    };
-                case SDLK_t:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::T,
-                        {-1, -1, -1}
-                    };
-                case SDLK_u:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::U,
-                        {-1, -1, -1}
-                    };
-                case SDLK_v:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::V,
-                        {-1, -1, -1}
-                    };
-                case SDLK_w:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::W,
-                        {-1, -1, -1}
-                    };
-                case SDLK_x:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::X,
-                        {-1, -1, -1}
-                    };
-                case SDLK_y:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::Y,
-                        {-1, -1, -1}
-                    };
-                case SDLK_z:
-                    return {
-                        EventType::KEY_PRESSED,
-                        Key::Z,
-                        {-1, -1, -1}
-                    };
+            case SDLK_UP: return { EventType::KEY_PRESSED, Key::UP, {-1, -1, -1} };
+            case SDLK_DOWN: return { EventType::KEY_PRESSED, Key::DOWN, {-1, -1, -1} };
+            case SDLK_LEFT: return { EventType::KEY_PRESSED, Key::LEFT, {-1, -1, -1} };
+            case SDLK_RIGHT: return { EventType::KEY_PRESSED, Key::RIGHT, {-1, -1, -1} };
+            case SDLK_SPACE: return { EventType::KEY_PRESSED, Key::SPACE, {-1, -1, -1} };
+            case SDLK_RETURN: return { EventType::KEY_PRESSED, Key::ENTER, {-1, -1, -1} };
+            case SDLK_ESCAPE: return { EventType::KEY_PRESSED, Key::ESCAPE, {-1, -1, -1} };
+            case SDLK_BACKSPACE: return { EventType::KEY_PRESSED, Key::BACKSPACE, {-1, -1, -1} };
+            case SDLK_F1: return { EventType::KEY_PRESSED, Key::F1, {-1, -1, -1} };
+            case SDLK_F2: return { EventType::KEY_PRESSED, Key::F2, {-1, -1, -1} };
+            case SDLK_F3: return { EventType::KEY_PRESSED, Key::F3, {-1, -1, -1} };
+            case SDLK_F4: return { EventType::KEY_PRESSED, Key::F4, {-1, -1, -1} };
+            case SDLK_F5: return { EventType::KEY_PRESSED, Key::F5, {-1, -1, -1} };
+            case SDLK_F6: return { EventType::KEY_PRESSED, Key::F6, {-1, -1, -1} };
+            case SDLK_F7: return { EventType::KEY_PRESSED, Key::F7, {-1, -1, -1} };
+            case SDLK_F8: return { EventType::KEY_PRESSED, Key::F8, {-1, -1, -1} };
+            case SDLK_F9: return { EventType::KEY_PRESSED, Key::F9, {-1, -1, -1} };
+            case SDLK_F10: return { EventType::KEY_PRESSED, Key::F10, {-1, -1, -1} };
+            case SDLK_F11: return { EventType::KEY_PRESSED, Key::F11, {-1, -1, -1} };
+            case SDLK_F12: return { EventType::KEY_PRESSED, Key::F12, {-1, -1, -1} };
+            case SDLK_a: return { EventType::KEY_PRESSED, Key::A, {-1, -1, -1} };
+            case SDLK_b: return { EventType::KEY_PRESSED, Key::B, {-1, -1, -1} };
+            case SDLK_c: return { EventType::KEY_PRESSED, Key::C, {-1, -1, -1} };
+            case SDLK_d: return { EventType::KEY_PRESSED, Key::D, {-1, -1, -1} };
+            case SDLK_e: return { EventType::KEY_PRESSED, Key::E, {-1, -1, -1} };
+            case SDLK_f: return { EventType::KEY_PRESSED, Key::F, {-1, -1, -1} };
+            case SDLK_g: return { EventType::KEY_PRESSED, Key::G, {-1, -1, -1} };
+            case SDLK_h: return { EventType::KEY_PRESSED, Key::H, {-1, -1, -1} };
+            case SDLK_i: return { EventType::KEY_PRESSED, Key::I, {-1, -1, -1} };
+            case SDLK_j: return { EventType::KEY_PRESSED, Key::J, {-1, -1, -1} };
+            case SDLK_k: return { EventType::KEY_PRESSED, Key::K, {-1, -1, -1} };
+            case SDLK_l: return { EventType::KEY_PRESSED, Key::L, {-1, -1, -1} };
+            case SDLK_m: return { EventType::KEY_PRESSED, Key::M, {-1, -1, -1} };
+            case SDLK_n: return { EventType::KEY_PRESSED, Key::N, {-1, -1, -1} };
+            case SDLK_o: return { EventType::KEY_PRESSED, Key::O, {-1, -1, -1} };
+            case SDLK_p: return { EventType::KEY_PRESSED, Key::P, {-1, -1, -1} };
+            case SDLK_q: return { EventType::KEY_PRESSED, Key::Q, {-1, -1, -1} };
+            case SDLK_r: return { EventType::KEY_PRESSED, Key::R, {-1, -1, -1} };
+            case SDLK_s: return { EventType::KEY_PRESSED, Key::S, {-1, -1, -1} };
+            case SDLK_t: return { EventType::KEY_PRESSED, Key::T, {-1, -1, -1} };
+            case SDLK_u: return { EventType::KEY_PRESSED, Key::U, {-1, -1, -1} };
+            case SDLK_v: return { EventType::KEY_PRESSED, Key::V, {-1, -1, -1} };
+            case SDLK_w: return { EventType::KEY_PRESSED, Key::W, {-1, -1, -1} };
+            case SDLK_x: return { EventType::KEY_PRESSED, Key::X, {-1, -1, -1} };
+            case SDLK_y: return { EventType::KEY_PRESSED, Key::Y, {-1, -1, -1} };
+            case SDLK_z: return { EventType::KEY_PRESSED, Key::Z, {-1, -1, -1} };
             }
         }
         if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
